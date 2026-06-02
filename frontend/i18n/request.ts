3f847b979a8375c4bb586731e0c5fb378fd99460
albinzena11 <150455242+albinzena11@users.noreply.github.com>
2026-06-02 22:37:01 +0200

    enhance internationalization support (EN, DE, SQ)

--- frontend/i18n/request.ts
+++ frontend/i18n/request.ts
@@ -1,18 +1,17 @@
 import { getRequestConfig } from 'next-intl/server';
-import { routing } from '@/frontend/i18n/routing';
+import { routing } from '@/i18n/routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
     // This typically corresponds to the `[locale]` segment
     let locale = await requestLocale;
 
     // Ensure that a valid locale is used
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if (!locale || !routing.locales.includes(locale as any)) {
+    if (!locale || !routing.locales.includes(locale as "sq" | "en" | "de")) {
         locale = routing.defaultLocale;
     }
 
     return {
-        locale: locale as string,
+        locale: locale,
         messages: (await import(`@/messages/${locale}.json`)).default
     };
 });
